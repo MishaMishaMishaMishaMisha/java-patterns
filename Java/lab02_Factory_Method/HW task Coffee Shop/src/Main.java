05ed755f4cd093c61cce57e72958c5b277a7c536
MishaMishaMishaMishaMisha <misha162534@gmail.com>
2023-02-19 15:03:48 +0200

    Complete task with Equation and some task about Coffee but not finished

---
+++ Java/lab02_Factory_Method/HW task Coffee Shop/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        CoffeeShop shop = new CoffeeShop();
+        shop.addIngridient("ing1", 2);
+        shop.addIngridient("ing2", 1);
+        shop.addIngridient("ing3", 0.5);
+        shop.addIngridient("ing4", 1.3);
+        shop.addIngridient("ing5", 2.2);
+
+
+
+
+    }
+}
