507c1bffe20ad8dff2876ff5c33bf0514cdd312a
MishaMishaMishaMishaMisha <misha162534@gmail.com>
2023-03-09 13:48:56 +0200

    comlete HW task Adapter_Charger

---
+++ Java/NEW_lab07_Adapter/task_3_2/src/UkrCarCalculator.java
@@ -0,0 +1,10 @@
+import com.customs.Customs;
+import com.customs.Auto;
+
+public class UkrCarCalculator implements Customs {
+    public float vehiclePrice(Auto var1) {
+
+    }
+
+    float tax(Auto var1);
+}
