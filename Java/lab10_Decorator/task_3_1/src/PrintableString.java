3b377899b8736bee51f45f7e9e97030395f7b533
MishaMishaMishaMishaMisha <misha162534@gmail.com>
2023-03-18 13:23:11 +0200

    complete hw_task decorator_printable_string

--- Java/lab10_Decorator/task_3_1/src/PrintableString.java
+++ Java/lab10_Decorator/task_3_1/src/PrintableString.java
@@ -1,10 +1,11 @@
-public class PrintableString {
+public class PrintableString implements Component {
     private final String base;
 
     public PrintableString(String base) {
         this.base = base;
     }
 
+    @Override
     public void print() {
         System.out.print(base);
     }
