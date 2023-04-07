edbfbd25a1209754ff51dd0e3185bb3615184660
MishaMishaMishaMishaMisha <misha162534@gmail.com>
2023-04-07 13:25:48 +0300

    complete hw_task openFiles

--- Java/lab14_Chain/task_2_1/src/Client.java
+++ Java/lab14_Chain/task_2_1/src/Client.java
@@ -1,5 +1,3 @@
-
-
 public class Client {
     public static void main(String[] args) {
 
@@ -12,12 +10,23 @@ public class Client {
                 "table.xlsx",
                 "presentation.pptx",
                 "document.pdf",
+                "textfile.txt"
         };
+
+        Handler handlerImage = new HandlerImage();
+        Handler handlerAdobe = new HandlerAdobe();
+        Handler handlerExcel = new HandlerExcel();
+        Handler handlerWord = new HandlerWord();
+        Handler handlerPowerPoint = new HandlerPowerPoint();
+
+        handlerImage.setNext(handlerAdobe);
+        handlerAdobe.setNext(handlerExcel);
+        handlerExcel.setNext(handlerWord);
+        handlerWord.setNext(handlerPowerPoint);
 
         // Create Chain of responsibility to open file by correct program according to the extension.
         for (String file : files) {
-            // open file
-            // handler.open(file);
+            handlerImage.open(file);
         }
 
 
